e3fb670856a1a69e4dc46ea5abb4370a3abe549a
mylit <vlmatveev5@gmail.com>
2023-12-11 18:09:58 +0200

    fix form contact

--- sendmail.php
+++ sendmail.php
@@ -1,30 +1,36 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = strip_tags(trim($_POST["name"]));
-    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $phone = trim($_POST["phone"]);
-    $message = trim($_POST["message"]);
+    // Собираем данные из формы
+    $name = htmlspecialchars(strip_tags(trim($_POST['name'])));
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+    $phone = htmlspecialchars(strip_tags(trim($_POST['phone'])));
+    $message = htmlspecialchars(strip_tags(trim($_POST['message'])));
 
-    // Проверка заполнения полей формы
-    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Здесь можно добавить код для обработки ошибки
+    // Проверка корректности email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Ошибка валидации email
         exit;
     }
 
-    // Настройка отправки сообщения
-    $recipient = "vlmatveev5@gmail.com"; // Замените на ваш адрес электронной почты
-    $subject = "Новое сообщение с вашего сайта от $name";
+    // Содержимое письма
     $email_content = "Имя: $name\n";
     $email_content .= "Email: $email\n";
     $email_content .= "Телефон: $phone\n\n";
     $email_content .= "Сообщение:\n$message\n";
 
-    $email_headers = "From: $name <$email>";
+    // Заголовки письма
+    $headers = "From: $name <$email>";
 
     // Отправка письма
-    mail($recipient, $subject, $email_content, $email_headers);
+    $to = 'vlmatveev5@gmail.com'; // Укажите ваш email
+    $subject = 'Новое сообщение с вашего сайта';
 
-    // Перенаправление обратно на форму с сообщением об успехе (необязательно)
-    header("Location: your-website.com/thankyou.html");
+    if (mail($to, $subject, $email_content, $headers)) {
+        // Письмо успешно отправлено
+        header('Location: thank_you_page.html');
+    } else {
+        // Не удалось отправить письмо
+        header('Location: error_page.html');
+    }
 }
 ?>
