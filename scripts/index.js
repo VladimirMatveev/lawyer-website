650da9fe798d4d14747cdbc5d5d108bae97f5a92
mylit <vlmatveev5@gmail.com>
2023-12-24 19:02:43 +0200

    fix send form

--- scripts/index.js
+++ scripts/index.js
@@ -33,6 +33,7 @@ document.getElementById('submitFormButton').addEventListener('click', function()
   xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status == 200) {
           onFormSuccess()
+          clearForm()
       } else if (xhr.readyState == 4) {
           alert('Ошибка при отправке сообщения.');
       }
@@ -44,4 +45,27 @@ function onFormSuccess () {
   const button = document.getElementById('submitFormButton')
   button.classList.add('success')
   button.innerText = 'Данные отправлены'
+
+}
+
+function clearForm() {
+  // Получаем форму по её идентификатору
+  var form = document.getElementById('contactForm');
+
+  // Находим все элементы input в форме
+  var inputs = form.querySelectorAll('input');
+  // Проходим по каждому элементу input и очищаем его
+  inputs.forEach(function(input) {
+      // Очищаем только текстовые поля, поля электронной почты и телефона
+      if(input.type == 'text' || input.type == 'email' || input.type == 'tel') {
+          input.value = '';
+      }
+  });
+
+  // Находим все элементы textarea в форме
+  var textareas = form.querySelectorAll('textarea');
+  // Проходим по каждому элементу textarea и очищаем его
+  textareas.forEach(function(textarea) {
+      textarea.value = '';
+  });
 }
